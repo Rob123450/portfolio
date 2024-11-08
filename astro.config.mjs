9514de578239875d202402f77765e71aec6197a1
Rob123450 <cedenor707@gmail.com>
2024-11-08 13:05:11 -0500

    Added temporary website links/paragraphs to be filled in later

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Rob123450.github.io',
+    site: 'https://Rob123450.github.io/portfolio',
 });
